91359f615e47b15d710210fa90eb1f83b71583f4
Anshika <anshikay2104@gmail.com>
2025-12-01 09:45:24 +0530

    jsconfig

--- src/app/(site)/auth/signin/page.tsx
+++ src/app/(site)/auth/signin/page.tsx
@@ -1,6 +1,7 @@
 // no need for "use client"
 
-import SignInModal from "@/components/Auth/signin";
+import SignInModal from "../../../../components/Auth/signin";
+
 
 export default function SignInPage() {
   return <SignInModal />;
